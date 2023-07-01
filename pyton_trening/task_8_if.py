5e07d4b42e5a7bf3909201ad2e2b900c5e042593
AndreyLomilin <z.lentisiz@yandex.ru>
2023-07-01 13:06:40 +0300

    first commit commit readme

---
+++ pyton_trening/task_8_if.py
@@ -0,0 +1,12 @@
+# num = 3
+# if num >= 0:
+#     print("число больше либо равно 0")
+# else:
+#     print("число отрицательное")
+
+str_1 = "test"
+str_2 = "Test1"
+if str_1 in str_2:
+    print("Да")
+else:
+    print("Нет")
---
+++ pyton_trening/task_8_if_elif.py
@@ -0,0 +1,17 @@
+num = 3.4
+if num > 0:
+    print("Положительное число")
+elif num == 0:
+    print("Ноль")
+else:
+    print("Чмсло отрицательное")
+
+permit_print = True
+if num > 0 and permit_print:
+    print("num - положительное число")
+elif not  permit_print:
+    print("печать завершена")
+
+number = a
+
+
